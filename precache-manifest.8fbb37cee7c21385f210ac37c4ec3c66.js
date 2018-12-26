e4d35dd9815dd2c10a0a9b6c42e75832cc14f23b
Christopher-Han <christopherhan4@gmail.com>
2018-12-26 14:46:23 -0500

    Updates

---
+++ precache-manifest.8fbb37cee7c21385f210ac37c4ec3c66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03102fabc2ce110987f8",
+    "url": "/static/js/main.03102fab.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "03102fabc2ce110987f8",
+    "url": "/static/css/main.10e20960.chunk.css"
+  },
+  {
+    "revision": "9272f19955b90ded07b902991e069112",
+    "url": "/index.html"
+  }
+];
